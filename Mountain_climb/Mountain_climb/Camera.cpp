cee3155fa2801fc059b4b8b18056c2ff46e3fb7a
shiHawk <166367650+shiHawk@users.noreply.github.com>
2024-12-02 18:53:15 +0900

    up

--- Mountain_climb/Mountain_climb/Camera.cpp
+++ Mountain_climb/Mountain_climb/Camera.cpp
@@ -1,1 +1,30 @@
+#include "DxLib.h"
 #include "Camera.h"
+#include "Player.h"
+#include "game.h"
+
+
+const float CameraScrollRangeH = 300;
+
+void InitCamera(Camera& camera)
+{
+	camera.pos = VGet(0, 0, 0);
+}
+
+void UpdateCamera(Camera& camera, const Player& player)
+{
+	if (player.GetTop() > camera.pos.y + (CameraScrollRangeH * 0.5f))
+	{
+		camera.pos.y = player.GetTop() - (CameraScrollRangeH * 0.5f);
+	}
+	else if (player.GetTop() < camera.pos.y - (CameraScrollRangeH * 0.5f))
+	{
+		camera.pos.y = player.GetTop() + (CameraScrollRangeH * 0.5f);
+	}
+
+	camera.drawOffset.x = camera.pos.x * -1;
+	camera.drawOffset.y = camera.pos.y * -1;
+
+	camera.drawOffset.x = camera.drawOffset.x + (Game::kScreenWidth * 0.5f);
+	camera.drawOffset.y = camera.drawOffset.y + (Game::kScreenHeight * 0.5f);
+}
--- Mountain_climb/Mountain_climb/Camera.h
+++ Mountain_climb/Mountain_climb/Camera.h
@@ -1,5 +1,11 @@
 #pragma once
-class Camera
-{
-};
 
+struct Player;
+struct Camera
+{
+	VECTOR pos;
+	VECTOR drawOffset;
+}; 
+
+void InitCamera(Camera& camera);
+void UpdateCamera(Camera& camera,const Player& player);
